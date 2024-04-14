e87631cb5adc35f2a327cde288033009a20bda85
Sambit Mallick <112485050+SAMBITMALLICK2003@users.noreply.github.com>
2024-04-14 01:55:49 +0000

    new

--- C++_College_Assignments/practice_test.cpp
+++ C++_College_Assignments/practice_test.cpp
@@ -1,11 +1,40 @@
 #include<iostream>
 using namespace std;
-
-int factorial(int n)
+class exeption{
+    string msg;
+    public:
+        exeption(const string& msg):msg(msg){}
+        const string& what() const {return msg;}
+};
+long factorial(int n)
 {
+    if(n<0)
+    {
+        throw exeption("Negative number");
+    }
+    if(n>20)
+    {
+        throw exeption("Number more than 20");
+    }
     if(n==0)
     {
         return 1;
     }
     return n*factorial(n-1);
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    try
+    {
+        int a=factorial(n);
+        cout<<"Factorial of "<<n<<" is: "<<a<<endl;
+    }
+    catch(const exeption& e)
+    {
+        cout<<e.what()<<endl;
+    }
+    return 0;
 }
